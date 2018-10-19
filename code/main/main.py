c62653d569e047a59582646a63c1c0515456479e
jessexknight <jesse.x.knight@gmail.com>
2018-10-19 10:58:37 -0400

    add turnover to toy model, restyle params.json, omit attributable death for now

--- code/main/main.py
+++ code/main/main.py
@@ -11,25 +11,34 @@ import numpy as np
 
 def dxfun(X,t,P,dxout={}):
   dX = X*0
+  # births and deaths
+  dX.update(P['nu']*P['pz']*X.sum(), **{'hi':'S'}, accum=np.add)
+  dX.update(P['mu']*X, accum=np.subtract)
+  # attributable death
+  # dX.update(P['phi']*X.iselect(**{'hi':'I'}), **{'hi':'I'}, accum=np.subtract)
+  # turnover
+  Xi = X.expand(Space(X.space.dims+[modelutils.partner(X.space.dim('ii'))]),norm=False)
+  for ki in ['M','W']:
+    zeta = transmit.zeta_fun(P['nu'],P['mu'],P['pz'].islice(ki=ki),P['px'].islice(ki=ki))
+    XZk = Xi.iselect(ki=ki) * zeta
+    dX.update(XZk.isum('ip'),ki=ki,accum=np.subtract)
+    dX.update(XZk.isum('ii'),ki=ki,accum=np.add)
   # force of infection
   lam = transmit.lambda_fun(X,P['C'],P['beta'],P['eps'],dxout).isum('p')
-  transmit.transfer(dX, src={'hi':'S'}, dst={'hi':'I'},
-    N = lam.iselect(hi='S')*X.iselect(hi='S'))
+  transmit.transfer(dX, src={'hi':'S'}, dst={'hi':'I'}, N = lam.iselect(hi='S')*X.iselect(hi='S'))
   # treatment
-  transmit.transfer(dX, src={'hi':'I'}, dst={'hi':'R'},
-    N = X.iselect(hi='I') * P['tau'])
+  transmit.transfer(dX, src={'hi':'I'}, dst={'hi':'R'}, N = X.iselect(hi='I') * P['tau'])
   # loss of immunity
-  transmit.transfer(dX, src={'hi':'R'}, dst={'hi':'S'},
-    N = X.iselect(hi='R') * P['gamma'])
-  # locals
+  transmit.transfer(dX, src={'hi':'R'}, dst={'hi':'S'}, N = X.iselect(hi='R') * P['gamma'])
+  # dxout
   dxout.update({v:locals()[v] for v in dxout if v in locals()})
   # return
   return dX
 
 def initfun(model,spaces):
   P = model.params
-  model.X0.update(P['N0']*P['px'],hi='S')
-  P['beta'] = P['beta'].expand(spaces['super'])
+  P['beta'] = P['beta'].expand(spaces['super']) # expand beta
+  model.X0.update(P['N0']*P['px'],hi='S') # initial condition
 
 def infectfun(sim):
   sim.init_x(transmit.transfer(
@@ -48,11 +57,8 @@ model = Model(X0 = Array(0,spaces['index']),
              params = params,
              initfun = lambda model: initfun(model,spaces))
 # sim = model.equilibriate()
-t = np.around(np.arange(1975, 2025+1e-6, 0.1),6)
+t = np.around(np.arange(1975, 2025+1e-6, 1),6)
 sim = infectfun(Simulation(model,t))
 sim.solve()
-sim.plot(selectors=[select[name] for name in ['S','I','R']])
+# sim.plot(selectors=[select[name] for name in ['S','I','R']])
 sim.plot(selectors=[select[name] for name in ['WH','MH','WM','MM','WL','ML']])
-
-# P = np.array([0.4,0.3,0.2,0.1])
-# print(zetafun(0.3,0.2,P,P,[4,np.nan,np.nan,np.nan]))
