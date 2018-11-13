695bacf992e8243b9e65edc30b532408e9d0cd8f
jessexknight <jesse.x.knight@gmail.com>
2018-11-13 12:33:40 -0500

    add Outputs, gif plotting framework

--- code/main/main.py
+++ code/main/main.py
@@ -4,6 +4,7 @@ config.plot()
 # epi-model imports
 from space import *
 from simulation import *
+import outputfuns
 import initutils
 import transmit
 import test
@@ -18,13 +19,12 @@ def dxfun(X,t,P,dxout={}):
   # turnover
   Xi = X.expand(Space(X.space.dims+[modelutils.partner(X.space.dim('ii'))]),norm=False)
   for ki in ['M','W']:
-    zeta = test.zeta_fun(P['nu'],P['mu'],P['pe'].islice(ki=ki),P['px'].islice(ki=ki))
-    XZk = Xi.iselect(ki=ki) * zeta
+    XZk = Xi.iselect(ki=ki) * P['zeta'].iselect(ki=ki)
     dX.update(XZk.isum('ip'),ki=ki,accum=np.subtract)
     dX.update(XZk.isum('ii'),ki=ki,accum=np.add)
   # force of infection
-  lam = transmit.lambda_fun(X,P['C'],P['beta'],P['eps'],dxout).isum('p')
-  transmit.transfer(dX, src={'hi':'S'}, dst={'hi':'I'}, N = lam.iselect(hi='S')*X.iselect(hi='S'))
+  lamp = transmit.lambda_fun(X,P['C'],P['beta'].islice(t=t),P['eps'],dxout)
+  transmit.transfer(dX, src={'hi':'S'}, dst={'hi':'I'}, N = lamp.isum('p').iselect(hi='S')*X.iselect(hi='S'))
   # treatment
   transmit.transfer(dX, src={'hi':'I'}, dst={'hi':'T'}, N = X.iselect(hi='I') * P['tau'])
   # # full recovery -> for SIT model, we have no recovery
@@ -36,8 +36,16 @@ def dxfun(X,t,P,dxout={}):
 
 def initfun(model,spaces):
   P = model.params
-  P['beta'] = P['beta'].expand(spaces['super']) * P['f-beta-h'].expand(spaces['super'])
-  model.X0.update(P['N0']*P['px'],hi='S') # initial condition
+  # beta
+  sbeta = spaces['super'].union(P['f-beta-h'].space)
+  P['beta'] = P['beta'].expand(sbeta) * P['f-beta-h'].expand(sbeta)
+  # turnover
+  for ki in ['M','W']:
+    P['zeta'].update(transmit.zeta_fun( P['nu'], P['mu'],
+        P['pe'].islice(ki=ki), P['px'].islice(ki=ki), P['dur'].islice(ki=ki)),
+      ki=ki)
+  # initial condition
+  model.X0.update(P['N0']*P['px'],hi='S')
 
 def infectfun(sim):
   sim.init_x(transmit.transfer(
@@ -47,6 +55,41 @@ def infectfun(sim):
     both = {'t':sim.t[0]},
     N = 1))
   return sim
+
+def get_outputs(spaces,select,t=None,names=None):
+  def tspace(space):
+    return space.union(Space([modelutils.tdim(flatten(t))]))
+  outputs = [
+    Output('N',
+           space = tspace(spaces['index']),
+           fun = lambda sim: sim.X,
+           accum = np.sum,
+           wax = False),
+    Output('X',
+           space = tspace(spaces['index']),
+           fun = lambda sim: sim.X / sim.X.isum('t',keep=True),
+           accum = np.sum,
+           wax = False),
+    Output('prevalence',
+           space = tspace(spaces['index']),
+           fun = lambda sim: outputfuns.prevalence(sim),
+           accum = np.average,
+           wax = True),
+    Output('incidence',
+          space = tspace(spaces['super'].subspace(['ki','ii','p'],keep=True)),
+          fun = lambda sim,t,lamp: outputfuns.incidence(sim,lam=lamp,t=t,per=1000,s='S'),
+          accum = np.average,
+          wax = True,
+          calc = 'peri', dxout = ['lamp']),
+    Output('cum-infect',
+          space = tspace(spaces['super'].subspace(['ki','ii','p'],keep=True)),
+          fun = lambda sim,t,lamp: outputfuns.incidence(sim,lam=lamp,t=t,per=False),
+          accum = np.sum,
+          wax = False,
+          calc = 'peri', dxout = ['lamp'], cum = True),
+  ]
+  names = [output.name for output in outputs] if names is None else flatten(names)
+  return xdict(xfilter(outputs,name=names))
 
 specdir = os.path.join(config.path['root'],'code','main','specs')
 dims   = initutils.objs_from_json(Dimension,            os.path.join(specdir,'dimensions.json'))
@@ -60,11 +103,36 @@ model = Model(X0 = Array(0,spaces['index']),
              select = select,
              initfun = lambda model: initfun(model,spaces))
 
-model.params.collapse(accum,idxs=['ii'])
-# sim = model.equilibriate()
+# model.params.collapse(accum,idxs=['ii'])
+# model.params['zeta']
+
 t = np.around(np.arange(1975, 2025+1e-6, 0.1),6)
-sim = infectfun(Simulation(model,t))
-sim.solve()
-sim.X *= (sim.X.isum('t',keep=True))**-1
-sim.plot(selectors=[model.select[name] for name in ['S','I','T']])
-sim.plot(selectors=[model.select[name] for name in ['WH','MH','WM','MM','WL','ML']])
+outputs = get_outputs(spaces,select,t=t,names=['N','X','prevalence'])
+sim = infectfun(Simulation(model,t,outputs=outputs))
+
+# # 1-off plotting
+# sim.solve()
+# sim.plot(outputs=['X'],selectors=[model.select[name] for name in ['S','I','T']])
+# sim.plot(outputs=['incidence'],selectors=[model.select[name] for name in ['WH','MH','WM','MM','WL','ML']])
+
+# # gif plotting
+def gifplot(dur):
+  # sim.model.params['nu'].update(nu)
+  sim.model.params['dur'].update(dur,ki='W',ii='H')
+  sim.init_model(sim.model)
+  sim.init_params()
+  sim.solve()
+  sim.plot(outputs=['prevalence'],
+           # selectors=[model.select[name] for name in ['S','I','T']],
+           selectors=[model.select[name] for name in ['WH','MH','WM','MM','WL','ML']],
+           show=False)
+  import matplotlib.pyplot as plt
+  plt.gca().set_ylim([0,1])
+
+# using decorator from utils
+gif(fname='test.gif',clf=True,verbose=True,overlay=(0.05,0.95),
+    # nu=np.linspace(0.03,0.06,5),
+    dur=np.linspace(5,15,5),
+    # eps=np.linspace(0,1,6),
+  )(gifplot)
+    
